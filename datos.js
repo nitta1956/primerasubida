53cbfb53006c1ca6d8e426ef068904782aac998a
nitta1956 <nitta1956@yahoo.es>
2019-05-11 21:59:27 -0500

    proyecto1

---
+++ datos.js
@@ -0,0 +1,28 @@
+const opciones = {
+     nombre: {
+     	alias: 'n',
+     	demand: true
+     },
+
+     matematicas: {
+     	demand: true,
+     	alias: 'm'
+     },
+
+     programacion: {
+     	demand: true,
+     	alias: 'p'
+     }
+
+}
+
+let obtenerPromedio = (nota_uno,nota_dos,nota_tres) => (nota_uno + nota_dos + nota_tres) /3;
+
+const argv =require('yargs')
+.command ('promedio','Calcular el promedio',opciones)
+.argv
+
+module.exports = {
+	obtenerPromedio,
+	argv
+};
